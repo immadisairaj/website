b64cdf220137ba887a592ac71a9385acd681c9eb
immadisairaj <immadirajendra.sai@gmail.com>
2022-02-25 18:22:08 +0530

    remove navigation animation

--- lib/router/site_router_delegate.dart
+++ lib/router/site_router_delegate.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:website/router/site_route_path.dart';
+import 'package:website/router/site_transition_delegate.dart';
 import 'package:website/screens/about/about.dart';
 import 'package:website/screens/home/home.dart';
 import 'package:website/screens/splash_screen/splash_screen.dart';
@@ -12,47 +13,51 @@ class SiteRouterDelegate extends RouterDelegate<SiteRoutePath>
 
   SiteRouterDelegate() : navigatorKey = GlobalKey<NavigatorState>();
 
-  bool show404 = false;
-  bool isHome = false;
-  bool isAbout = false;
+  final SiteTransitionDelegate _siteTransitionDelegate =
+      SiteTransitionDelegate();
+
+  bool _show404 = false;
+  bool _isHome = false;
+  bool _isAbout = false;
 
   @override
   Widget build(BuildContext context) {
     return Navigator(
       key: navigatorKey,
+      transitionDelegate: _siteTransitionDelegate,
       pages: [
         MaterialPage(
           key: const ValueKey('Base'),
           child: Container(),
         ),
-        if (show404)
+        if (_show404)
           const MaterialPage(
               key: ValueKey('UnknownPage'), child: UnknownScreen()),
-        if (!isHome && !isAbout && !show404)
+        if (!_isHome && !_isAbout && !_show404)
           MaterialPage(
             key: const ValueKey('SplashPage'),
             child: SplashScreen(
               onHomeNav: () {
-                if (!show404 && !isHome && !isAbout) {
-                  isHome = true;
-                  isAbout = false;
+                if (!_show404 && !_isHome && !_isAbout) {
+                  _isHome = true;
+                  _isAbout = false;
                   notifyListeners();
                 }
               },
             ),
           ),
-        if (isHome || isAbout)
+        if (_isHome || _isAbout)
           MaterialPage(
             key: const ValueKey('HomePage'),
             child: Home(
               onAboutPress: () {
-                isHome = false;
-                isAbout = true;
+                _isHome = false;
+                _isAbout = true;
                 notifyListeners();
               },
             ),
           ),
-        if (isAbout)
+        if (_isAbout)
           const MaterialPage(key: ValueKey('AboutPage'), child: About()),
       ],
       onPopPage: (route, result) {
@@ -60,8 +65,8 @@ class SiteRouterDelegate extends RouterDelegate<SiteRoutePath>
           return false;
         }
 
-        isHome = true;
-        isAbout = false;
+        _isHome = true;
+        _isAbout = false;
         notifyListeners();
 
         return true;
@@ -71,14 +76,14 @@ class SiteRouterDelegate extends RouterDelegate<SiteRoutePath>
 
   @override
   SiteRoutePath get currentConfiguration {
-    if (show404) {
+    if (_show404) {
       return SiteRoutePath.unknown();
     }
 
-    if (isHome) {
+    if (_isHome) {
       return SiteRoutePath.home();
     }
-    if (isAbout) {
+    if (_isAbout) {
       return SiteRoutePath.about();
     }
     return SiteRoutePath.splash();
@@ -87,23 +92,23 @@ class SiteRouterDelegate extends RouterDelegate<SiteRoutePath>
   @override
   Future<void> setNewRoutePath(SiteRoutePath configuration) async {
     if (configuration.isUnknown) {
-      isHome = false;
-      isAbout = false;
-      show404 = true;
+      _isHome = false;
+      _isAbout = false;
+      _show404 = true;
       return;
     }
 
     if (configuration.isAboutPage) {
-      isHome = false;
-      isAbout = true;
+      _isHome = false;
+      _isAbout = true;
     } else if (configuration.isHomePage) {
-      isHome = true;
-      isAbout = false;
+      _isHome = true;
+      _isAbout = false;
     } else if (configuration.isSplashPage) {
-      isHome = false;
-      isAbout = false;
+      _isHome = false;
+      _isAbout = false;
     }
 
-    show404 = false;
+    _show404 = false;
   }
 }
